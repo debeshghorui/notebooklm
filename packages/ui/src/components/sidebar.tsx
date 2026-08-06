414b5441416ab1c1c3a0d33ccc7cc5b665146581
debeshghorui <debeshghorui@gmail.com>
2026-08-07 02:45:58 +0530

    fix: update import path for useIsMobile in sidebar.tsx for better module resolution

--- packages/ui/src/components/sidebar.tsx
+++ packages/ui/src/components/sidebar.tsx
@@ -5,7 +5,7 @@ import { mergeProps } from "@base-ui/react/merge-props";
 import { useRender } from "@base-ui/react/use-render";
 import { cva, type VariantProps } from "class-variance-authority";
 
-import { useIsMobile } from "@/hooks/use-mobile";
+import { useIsMobile } from "@repo/ui/hooks/use-mobile";
 import { cn } from "@repo/ui/lib/utils";
 import { Button } from "@repo/ui/components/button";
 import { Input } from "@repo/ui/components/input";
